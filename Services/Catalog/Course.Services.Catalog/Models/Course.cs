0a9b78a3b495a1216d8ce64896cd2b44029aaef5
batu_ <batu_@DESKTOP-AOMM71G>
2022-04-28 15:06:35 +0300

    Models Add MongoDb.Driver package install one to many and one to one relations

---
+++ Services/Catalog/Course.Services.Catalog/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Course.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId] //Primary Key
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTiem { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore] //MongoDb'ye yansıtmıyoruz 
+        public Category Category { get; set; }
+    }
+}
