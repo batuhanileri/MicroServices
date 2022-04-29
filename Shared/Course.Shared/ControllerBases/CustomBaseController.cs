04adbc51310c9ea70f110d9b7d676c9773d3531b
batu_ <batu_@DESKTOP-AOMM71G>
2022-04-29 11:06:24 +0300

    CoursesController Added CustomBaseController Created

---
+++ Shared/Course.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Course.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Shared.ControllerBases
+{
+    public class CustomBaseController:ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
