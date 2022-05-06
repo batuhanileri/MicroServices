3eb5809582fc398fdb364a9d0c954b48f5af56dd
batu_ <batu_@DESKTOP-AOMM71G>
2022-05-06 18:00:46 +0300

    PhotoStock.API Authentication

--- Services/PhotoStock/Course.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/Course.Services.PhotoStock/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +27,18 @@ namespace Course.Services.PhotoStock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServerUrl"];
+                opt.Audience = "photo_stock_catalog";
+                opt.RequireHttpsMetadata = false;
+            });
 
-            services.AddControllers();
+
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Course.Services.PhotoStock", Version = "v1" });
@@ -44,7 +56,7 @@ namespace Course.Services.PhotoStock
             }
             app.UseStaticFiles(); //Bu middleware ile fotoðraflara public olarak dýþarýdan eriþim izni veriyoruz
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
