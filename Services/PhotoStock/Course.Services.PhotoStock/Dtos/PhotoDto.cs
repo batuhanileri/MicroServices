eafffe98ea7d382ba09de8a47a9ba806d42ae272
batu_ <batu_@DESKTOP-AOMM71G>
2022-05-06 17:32:40 +0300

    PhotoSave , PhotoDelete endpoint added

---
+++ Services/PhotoStock/Course.Services.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Services.PhotoStock.Dtos
+{
+    public class PhotoDto
+    {
+        public string url { get; set; }
+        public string Url { get; internal set; }
+    }
+}
