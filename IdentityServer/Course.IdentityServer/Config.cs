7b494819b40fda808246c8388075618815ff51f1
batu_ <batu_@DESKTOP-AOMM71G>
2022-05-06 12:50:19 +0300

    Config - User Resource OpenId , Email , Profile, Roles Client Add

--- IdentityServer/Course.IdentityServer/Config.cs
+++ IdentityServer/Course.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Course.IdentityServer
@@ -20,6 +21,16 @@ namespace Course.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
+                       new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile(),
+                       new IdentityResource()
+                       {
+                           Name="roles",
+                           DisplayName="Roles",
+                           Description="Kullanıcı Rolleri",
+                           UserClaims = new[]{"role"}
+                       }
 
                    };
 
@@ -40,9 +51,27 @@ namespace Course.IdentityServer
                     ClientId="MvcClient",
                     ClientSecrets={new Secret("secret".Sha256())},
                     AllowedGrantTypes=GrantTypes.ClientCredentials,
-                    AllowedScopes={"catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+                    AllowedScopes={"catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }        
+                },
 
-                    
+                 new Client
+                {
+                    ClientName="Asp.Net Core Mvc",
+                    ClientId="MvcClientForUser",
+                    AllowOfflineAccess=true,
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={ 
+                     IdentityServerConstants.StandardScopes.Email,
+                     IdentityServerConstants.StandardScopes.OpenId,
+                     IdentityServerConstants.StandardScopes.Profile,
+                     IdentityServerConstants.StandardScopes.OfflineAccess, //Refresh Token için 
+                     "roles"
+                     },
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage=TokenUsage.ReUse
                 }
             };
     }
