eed77d50962f4b5b31b682eb7d685d8b1fa4f08d
batu_ <batu_@DESKTOP-AOMM71G>
2022-05-06 09:51:28 +0300

    SignUp Code

---
+++ IdentityServer/Course.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Course.IdentityServer.Models;
+using Course.IdentityServer.Models.Dtos;
+using Course.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+
+            };
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
+            }
+
+            return NoContent();
+        }
+
+    }
+}
